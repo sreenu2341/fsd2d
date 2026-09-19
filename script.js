239711a6ee6c5b2d80de64e0e01888da5884d2af
sreenu2341.com <sreenivasuluvvv724@gmail.com>
2026-09-19 13:39:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-import React, { useState } from "https://esm.sh/react@18";
-import { createRoot } from "https://esm.sh/react-dom@18/client";
-
+import React, { useState } from "react";
 function ConditionalRender() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h2", null, "Conditional Rendering"),
-
-    isLoggedIn
-      ? React.createElement("p", null, "Welcome, User!")
-      : React.createElement("p", null, "Please Login"),
-
-    React.createElement(
-      "button",
-      {
-        onClick: () => setIsLoggedIn(!isLoggedIn)
-      },
-      isLoggedIn ? "Logout" : "Login"
-    )
-  );
-}
-
-createRoot(document.getElementById("root")).render(
-  React.createElement(ConditionalRender)
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+return (
+<div>
+<h2>Conditional Rendering</h2>
+{isLoggedIn ? <p>Welcome, User! </p> : <p>Please Login</p>}
+<button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+{isLoggedIn ? "Logout" : "Login"}
+</button>
+</div>
 );
+}
+export default ConditionalRender;
